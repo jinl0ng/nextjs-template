0c97b944e00b8548bfb0d5f034bbce49dc96203b
jinlong.zhu <jinlong.zhu@datanebula.ai>
2024-10-02 08:44:17 +0800

    feat: setup some nextjs feature

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  logging: {
+    fetches: {
+      fullUrl: true,
+      hmrRefreshes: true,
+    },
+  },
+  experimental: {
+    ppr: "incremental",
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
