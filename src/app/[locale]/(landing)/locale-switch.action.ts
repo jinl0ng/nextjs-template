00f4594d5a763b5543307477e732fd9f1214c841
jinlong.zhu <jinlong.zhu@datanebula.ai>
2024-10-24 18:33:39 +0800

    feat: update locale switcher

--- src/app/[locale]/(landing)/locale-switch.action.ts
+++ src/app/[locale]/(landing)/locale-switch.action.ts
@@ -3,12 +3,16 @@
 import { cookies } from "next/headers";
 
 import { Locale, i18n } from "@/i18n/config";
+import { redirect } from "@/i18n/routing";
 
-export async function switchLocale(locale: Locale) {
+type Href = Parameters<typeof redirect>[0]["href"];
+
+export async function switchLocale(href: Href, locale: Locale) {
   const isLocaleValid = i18n.locales.includes(locale);
   if (!isLocaleValid) {
     throw new Error("Invalid locale");
   }
   const cookieStore = await cookies();
   cookieStore.set(i18n.cookieName, locale);
+  redirect({ href, locale });
 }
--- src/app/[locale]/(landing)/locale-switcher.tsx
+++ src/app/[locale]/(landing)/locale-switcher.tsx
@@ -1,20 +1,30 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
+
 import { useLocale, useTranslations } from "next-intl";
 
 import { Button } from "@/components/ui/button";
+import { usePathname } from "@/i18n/routing";
 
 import { switchLocale } from "./locale-switch.action";
 
 export default function LocaleSwitcher() {
   const t = useTranslations("LocaleSwitcher");
   const locale = useLocale();
+  const pathname = usePathname();
+  const query = useSearchParams();
 
   return (
     <Button
       variant="outline"
       size="sm"
-      onClick={() => switchLocale(locale === "en" ? "zh" : "en")}
+      onClick={() =>
+        switchLocale(
+          { pathname, query: Object.fromEntries(query) },
+          locale === "en" ? "zh" : "en"
+        )
+      }
     >
       {t("switchTo", { locale: locale === "en" ? "中文" : "English" })}
     </Button>
