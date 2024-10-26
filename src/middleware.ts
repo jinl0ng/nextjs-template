943854252d68f0d802bb3ade9eee74de8ec41899
jinlong.zhu <jinlong.zhu@datanebula.ai>
2024-10-26 11:27:36 +0800

    feat: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,10 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ["/", "/(zh|en)/:path*"],
+  matcher: [
+    // Match all pathnames except for
+    // - … if they start with `/api`, `/_next` or `/_vercel`
+    // - … the ones containing a dot (e.g. `favicon.ico`)
+    "/((?!_next|_vercel|monitoring*|sentry*|.*\\..*).*)",
+  ],
 };
